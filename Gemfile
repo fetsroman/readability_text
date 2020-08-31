d36e6e51421f759c69b516c08de25e8c23bd77f8
fetsroman <fets.roman@gmail.com>
2020-08-31 18:22:18 +0300

    Add administrate

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'rack-cors'
 
 gem 'jwt'
 gem 'detect_language'
+gem "administrate"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :texts
+      resources :users
+
+      root to: "texts#index"
+    end
   namespace 'api' do
     post '/sign_up', to: 'users#create'
     post '/sign_in', to: 'authentication#sign_in'
