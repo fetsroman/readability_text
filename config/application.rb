fc6f5ffd615c70367009196fcb210b97b6eff690
fetsroman <fets.roman@gmail.com>
2020-09-01 12:13:08 +0300

    Add auth to administrate

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module ReadabilityText
     # the framework and any gems in your application.
 
     # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
+    # Middleware like sessions, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :texts
-      resources :users
+    get '/sign_in', to: 'sessions#new'
+    post '/sign_in', to: 'sessions#create'
 
-      root to: "texts#index"
-    end
+    resources :texts
+    resources :users
+
+    root to: "texts#index"
+  end
   namespace 'api' do
     post '/sign_up', to: 'users#create'
     post '/sign_in', to: 'authentication#sign_in'
